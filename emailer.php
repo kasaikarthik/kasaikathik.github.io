b8feff13fc23df9b076fe5eb94c1fcb4e7aaaf60
Sai Karthik <kasaikarthik@gmail.com>
2020-07-19 19:38:26 +0530

    Modified contact me form

--- emailer.php
+++ emailer.php
@@ -1,16 +1,16 @@
 <?php 
-if(isset($_POST['submit'])){
+if(isset($_GET['submit'])){
     $to = "hello@thesaikatrist.com"; // this is my Email address
-    $from = $_POST['email']; // this is the sender's Email address
-    $name = $_POST['name'];
+    $from = $_GET['email']; // this is the sender's Email address
+    $name = $_GET['name'];
     $subject = "Katrist Website - Message from" . $name;
     $subject2 = "Copy of your Message to Sai Karthik";
-    $message = $name . " wrote the following:" . "\n\n" . $_POST['message'];
-    $message2 = "Hey there! Here is a copy of the message that you sent to me:" . "\n" . $name . "\n\n" . $_POST['message'];
+    $message1 = $name . " wrote the following:" . "\n\n" . $_GET['message'];
+    $message2 = "Hey there! Here is a copy of the message that you sent to me:" . "\n" . $name . "\n\n" . $_GET['message'];
 
     $headers = "From:" . $from;
     $headers2 = "From:" . $to;
-    mail($to,$subject,$message,$headers);
+    mail($to,$subject,$message1,$headers);
     mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender
     echo "Mail Sent. Thank you " . $name . ", I'll contact you shortly.";
     }
